7c87117e73bf6e0c6cac47ec68b704a2a6370e07
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-12-17 10:20:12 +0530

    last-commmit

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const prompt = require("prompt-sync")()
 const fs = require("fs")
 
-const text=  prompt("enter the data:- ")
+const text = prompt("enter the data:- ")
 fs.writeFileSync("example.txt", text)
 console.log("file created successsfully");
 
 
-const data=
-fs.readFileSync("example.txt", "utf-8")
+const data = fs.readFileSync("example.txt", "utf-8")
 console.log(data);
+
+
+
+
+
+fs.appendFileSync("example.txt", " amit")
+
+
+fs.unlinkSync("example.txt")
+if (!"example.txt") {
+  console.log("file not exist");
+}
+else {
+  console.log("file delete successful");
+
+}
+
+const file = prompt("enter the file:- ")
+if (fs.existsSync(file)) {
+  console.log(`this file is  exist ${file}`);
+  
+
+}
+else {
+  console.log("file  doest not exist");
+
+}
