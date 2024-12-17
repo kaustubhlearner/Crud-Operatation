a73e8c952f72b293cd702ec11143677600081092
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-12-17 20:46:25 +0530

    third-commit

--- script.js
+++ script.js
@@ -6,32 +6,34 @@ fs.writeFileSync("example.txt", text)
 console.log("file created successsfully");
 
 
-const data = fs.readFileSync("example.txt", "utf-8")
-console.log(data);
+// const data = fs.readFileSync("example.txt", "utf-8")
+// console.log(data);
 
 
 
 
 
-fs.appendFileSync("example.txt", " amit")
+// fs.appendFileSync("example.txt", " amit")
 
 
-fs.unlinkSync("example.txt")
-if (!"example.txt") {
-  console.log("file not exist");
-}
-else {
-  console.log("file delete successful");
+// fs.unlinkSync("example.txt")
+// if (!"example.txt") {
+//   console.log("file not exist");
+// }
+// else {
+//   console.log("file delete successful");
 
-}
+// }
 
-const file = prompt("enter the file:- ")
-if (fs.existsSync(file)) {
-  console.log(`this file is  exist ${file}`);
+// const file = prompt("enter the file:- ")
+// if (fs.existsSync(file)) {
+//   console.log(`this file is  exist ${file}`);
   
 
-}
-else {
-  console.log("file  doest not exist");
+// }
+// else {
+//   console.log("file  doest not exist");
 
-}
+// }
+
+// here is the full code git 
